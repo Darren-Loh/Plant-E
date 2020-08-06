1b4f980b3319c319df343aa4cec4ff3e3a689c72
Darren Loh <vanhfdarren@gmail.com>
2020-08-06 18:20:45 +0800

    Added Fragments and included journal Fragment

---
+++ app/src/main/java/com/example/plant_e/JournalFragment.java
@@ -0,0 +1,154 @@
+package com.example.plant_e;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link JournalFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class JournalFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public JournalFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment JournalFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static JournalFragment newInstance(String param1, String param2) {
+        JournalFragment fragment = new JournalFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    //-------------------------outside-------------------------------
+    Map<String, JournalStore> journalStoreMap = new HashMap<>();
+
+
+    //----------------------------------------------------------------
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_journal, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        final TextView txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+        final TextView txtDate = (TextView) view.findViewById(R.id.txtDate);
+        final EditText txtDescription = (EditText) getActivity().findViewById(R.id.txtDescription);
+        ImageButton btnBackwards = view.findViewById(R.id.btnBackwards);
+        Button btnSave = view.findViewById(R.id.btnSave);
+        ImageButton btnForwards = view.findViewById(R.id.btnForwards);
+
+
+
+
+        // ----------------- Date formatting-------------------------------------------
+        final LocalDate[] dateObj = {LocalDate.now()};
+        final DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        String formattedDate = dateObj[0].format(myFormat);
+
+        txtDate.setText(formattedDate);
+        final JournalStore journalInstance = new JournalStore(formattedDate);  //date instance
+
+        //----------Hashmap-------------------------------------------
+//        if (journalStoreMap.get(txtDate.getText().toString()) != null){
+//            txtDescription.setText(journalStoreMap.get(txtDate.getText().toString()).getDescription());  // if Date instance already inside Hashmap
+//        }
+
+        //--------------btnBackwards onClickListener----------------------------------------
+        btnBackwards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dateObj[0] = dateObj[0].minusDays(1);
+                txtDate.setText(dateObj[0].format(myFormat));
+
+                if (journalStoreMap.get(txtDate.getText().toString()) != null){
+                    txtDescription.setText(journalStoreMap.get(txtDate.getText().toString()).getDescription());  // if Date instance already inside Hashmap
+                }
+
+            }
+        });
+
+        //---------------btnForwards onClickListener----------------------------------------
+        btnForwards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dateObj[0] = dateObj[0].plusDays(1);
+                txtDate.setText(dateObj[0].format(myFormat));
+
+                if (journalStoreMap.get(txtDate.getText().toString()) != null){
+                    txtDescription.setText(journalStoreMap.get(txtDate.getText().toString()).getDescription());  // if Date instance already inside Hashmap
+                }
+
+            }
+        });
+
+        //--------------btnSave onClickListener---------------------------------------------
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                journalStoreMap.put(txtDate.getText().toString(), new JournalStore(txtDate.getText().toString(), txtDescription.getText().toString()));
+
+            }
+        });
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/plant_e/MainActivity.java
+++ app/src/main/java/com/example/plant_e/MainActivity.java
@@ -1,18 +1,54 @@
 package com.example.plant_e;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView btmNavi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // testing to run
-        startActivity(new Intent(MainActivity.this, PlantJournal.class));
+        btmNavi = findViewById(R.id.btmNavi);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        btmNavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.video:
+                        selectedFragment = new LiveFeedFragment();
+                        break;
+                    case R.id.journal:
+                        selectedFragment = new JournalFragment();
+                        break;
+                    case R.id.research:
+                        selectedFragment = new PlantDiagnosisFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+
+
+                return true;
+            }
+        });
+
     }
 }
