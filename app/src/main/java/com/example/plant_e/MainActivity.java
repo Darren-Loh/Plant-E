391934d3d7beaaa801ac82344b948e8cb84fa624
Darren Loh <vanhfdarren@gmail.com>
2020-08-11 03:19:49 +0800

    Mandatory commit

--- app/src/main/java/com/example/plant_e/MainActivity.java
+++ app/src/main/java/com/example/plant_e/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //-----------------------------------------------------------------------------
-
+        
         //----------------Navigation Drawer-----------------------------------------
 
         drawer = findViewById(R.id.drawer_layout);
